7e0f6ba1ed34b3d74c490e59782184b400e568ad
mdalvi <milind.dalvi@protonmail.com>
2023-08-05 19:57:18 +0530

    doc strings and comments

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ from agents.linkedin import get_linkedin_profile_url
 from third_parties.linkedin import get_linkedin_profile, get_saved_linkedin_profile
 
 
-def run_chain_for_information():
+def run_chain_for_information() -> None:
+    """
+    Prints short summary of the provided information along with two interesting facts about the person
+    :return: None
+    """
     with open(Path("storage/wikipedia/donald.trump.txt"), "r") as f:
         information = f.read()
 
@@ -30,12 +34,30 @@ def run_chain_for_information():
     print(chain.run(information=information))
 
 
-def run_chain_for_linkedin():
+def run_chain_for_linkedin_url(name: str) -> str:
+    """
+    Searches and returns the LinkedIn profile page url of the person
+    :param name: Name of person to Google search
+    :return: profile page url
+    """
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    result = get_linkedin_profile_url(llm, name)
+    print(result)
+    return result
+
+
+def run_chain_for_linkedin(name: str) -> None:
+    """
+    Step 1: Searches google for LinkedIn URL of the person
+    Step 2: Fetches public data from LinkedIn about the person
+    Step 3: Generates summary and two interesting facts about the person using LLM
+    :return: None
+    """
     # profile = get_linkedin_profile(
-    #     profile_url="https://www.linkedin.com/in/milinddalvi/"
+    #     profile_url=run_chain_for_linkedin_url(name)
     # )
     # data = profile.content
-    # with open("storage/linkedin/milind.dalvi.json", "wb") as f:
+    # with open(f"storage/linkedin/{name}.json", "wb") as f:
     #     f.write(data)
     #
     information = get_saved_linkedin_profile("storage/linkedin/milind.dalvi.json")
@@ -43,18 +65,13 @@ def run_chain_for_linkedin():
     custom_template = """
         Given the LinkedIn information {information} about a person, create
         1. a short summary
-        2. two interesting questions to ask in interview to this person. 
+        2. two interesting facts about the person
         """
-    # facts about the person
+
     prompt = PromptTemplate(input_variables=["information"], template=custom_template)
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     chain = LLMChain(llm=llm, prompt=prompt)
     print(chain.run(information=information))
-
-
-def run_chain_for_linkedin_url():
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    print(get_linkedin_profile_url(llm, "Ranjan Pradhan Capgemini"))
 
 
 if __name__ == "__main__":
@@ -62,4 +79,4 @@ if __name__ == "__main__":
     load_dotenv()
 
     print("Hello LangChain!")
-    run_chain_for_linkedin_url()
+    run_chain_for_linkedin_url("Ranjan Pradhan Capgemini")
